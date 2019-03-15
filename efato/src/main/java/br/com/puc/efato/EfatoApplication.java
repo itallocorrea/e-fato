41581d842725f5adc1c6788bf990040ba98f55a1
itallocorrea <32783253+itallocorrea@users.noreply.github.com>
2019-03-15 19:59:19 -0300

    Inserindo estrutura do projeto

---
+++ efato/src/main/java/br/com/puc/efato/EfatoApplication.java
@@ -0,0 +1,13 @@
+package br.com.puc.efato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfatoApplication.class, args);
+	}
+
+}
